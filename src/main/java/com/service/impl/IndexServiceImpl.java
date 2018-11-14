a3287a197f64fa17b47381d632e611dfebc28cfb
ljh <email@ljh525.com>
2018-11-14 16:12:22 +0800

    gitignore set

--- src/main/java/com/service/impl/IndexServiceImpl.java
+++ src/main/java/com/service/impl/IndexServiceImpl.java
@@ -1,4 +1,46 @@
 package com.service.impl;
 
-public class IndexServiceImpl {
+import com.service.inter.IndexService;
+import com.util.ESClient;
+
+import java.util.List;
+
+public class IndexServiceImpl implements IndexService {
+
+
+    private ESClient client ;
+    @Override
+    public void index(String id) {
+
+    }
+
+    @Override
+    public void del(String id) throws Exception {
+
+    }
+
+    @Override
+    public Object get() {
+        return null;
+    }
+
+    @Override
+    public void update(String id) throws Exception {
+
+    }
+
+    @Override
+    public List<Object> mutiGet(String... ids) throws Exception {
+        return null;
+    }
+
+    @Override
+    public void bulk(String... ids) throws Exception {
+
+    }
+
+    @Override
+    public void bulkProcesstor(String index, String type, String... ids) throws Exception {
+
+    }
 }
